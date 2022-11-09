ffe207f8a1b8f5eac77574eb4b47ab5f5cddf3d2
chelloie <chloehol@email.unc.edu>
2022-11-09 14:26:19 -0500

    hw8

---
+++ homework 8/Harris Nuclear Plant - Emergency Planning Information_files/globalConfig.js
@@ -0,0 +1,8 @@
+window.globalConfig = {
+ApigeeServiceHost: 'https://cust-api.duke-energy.com',
+ApigeeServiceHostCC: 'https://cust-api.duke-energy.com',
+ApiHost: 'https://p-scapi.duke-energy.com',
+AppInsightsKey: '212113e0-6928-4b9e-8324-bec21c50fb76',
+SITECORE_API_KEY: '{9F777224-3275-4D56-BD29-371FB3C00821}',
+GoogleTagManagerId: 'GTM-KBRRX75',
+};
